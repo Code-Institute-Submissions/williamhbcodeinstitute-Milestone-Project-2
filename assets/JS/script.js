71614b4f15580cf07bd30dd7418bcd0f6eb4d2ee
EC2 Default User <ec2-user@ip-172-31-11-8.eu-west-1.compute.internal>
2019-09-04 08:45:12 +0000

    Added functioning google maps with location set to south east asia

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,8 +2,8 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 14.058324,
+            lng: 108.277199
         }
     });
 }
