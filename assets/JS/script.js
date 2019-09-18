91a1ef91d13ee331d29b0db94fd54b3be79e0a47
EC2 Default User <ec2-user@ip-172-31-11-8.eu-west-1.compute.internal>
2019-09-18 18:54:11 +0000

    Google places marker

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,71 +1,36 @@
 function initMap() {
-    var Hanoi = { lat: 21.027763, lng: 105.834160 };
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 6,
+    var locations = [
+    { lat: 10.823099, lng: 106.629662 },
+    { lat: 11.936230, lng: 108.445259 },
+    { lat: 16.075239, lng: 108.224136 },
+    { lat: 16.463713, lng: 107.590866 },
+    { lat: 21.027763, lng: 105.834160, }
+];
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 5,
         center: {
-            lat: 14.058324,
-            lng: 108.277199
+            lat: 16.058324,
+            lng: 108.277000
         }
     });
-    var contentString = '<div id="content">' +
-        '<div id="siteNotice">' +
-        '</div>' +
-        '<h1 id="firstHeading" class="firstHeading">Hanoi City</h1>' +
-        '<div id="bodyContent">' +
-        '<p><b>Hanoi</b>, is the <b>Capital City of Vietnam</b>, is known for centuries old architecture' +
-        'and a rich culture with Southeast Asian, Chinese and French influences' +
-        'At its heart is the chaotic Old Quarter, where the narrow streets are roughly arranged by trade. ' +
-        ' There are many little temples, including Bach Ma, honoring a legendary horse, plus Đồng Xuân Market,' +
-        'selling household goods and street food.</p>' +
-        '</div>' +
-        '</div>';
 
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
+    // Create an array of alphabetical characters used to label the markers.
+    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-    var marker = new google.maps.Marker({
-        position: Hanoi,
-        map: map,
-        title: 'Hanoi City'
-    });
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
-
-
-        var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
-        var locations = [{
-            lat: 10.823099,
-            lng: 106.629662
-        }, {
-            lat: 11.936230,
-            lng: 108.445259
-        }, {
-            lat: 16.075239,
-            lng: 108.224136
-        }, {
-            lat: 16.463713,
-            lng: 107.590866,
-        }, {
-            lat: 21.027763,
-            lng: 105.834160,
-        }];
-
-
-
-    });
-}
-
-function initPlaces() {
+    // Add some markers to the map.
+    // Note: The code uses the JavaScript Array.prototype.map() method to
+    // create an array of markers based on a given "locations" array.
+    // The map() method here has nothing to do with the Google Maps API.
     var markers = locations.map(function(location, i) {
         return new google.maps.Marker({
             position: location,
             label: labels[i % labels.length]
         });
     });
-    var markerCluster = new MarkerClusterer(map, markers, {
-        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-    });
+
+    // Add a marker clusterer to manage the markers.
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+
     var request = {
         placeId: 'ChIJLUKBJJerNTERCH5DEUBZgLg',
         fields: ['name', 'formatted_address', 'place_id', 'geometry']
@@ -89,3 +54,5 @@ function initPlaces() {
         }
     });
 }
+
+
