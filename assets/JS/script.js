a593c3d8c8040b4f8585cd17c0e204fce7e82a58
EC2 Default User <ec2-user@ip-172-31-11-8.eu-west-1.compute.internal>
2019-09-23 16:18:23 +0000

    updated Google markers script

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,58 +1,108 @@
-function initMap() {
-    var locations = [
-    { lat: 10.823099, lng: 106.629662 },
-    { lat: 11.936230, lng: 108.445259 },
-    { lat: 16.075239, lng: 108.224136 },
-    { lat: 16.463713, lng: 107.590866 },
-    { lat: 21.027763, lng: 105.834160, }
+var map;
+var InforObj = [];
+
+var centerCords = {
+    lat: 16.058324,
+    lng: 108.277000
+};
+
+var markersOnMap = [{
+        placeName: "Vietnam (Ho-Chi-Minh City)",
+        LatLng: [{
+            lat: 10.775844,
+            lng: 106.701756
+        }]
+    },
+    {
+        placeName: "Vietnam (Da Nang City)",
+        LatLng: [{
+            lat: 16.068000,
+            lng: 108.212000
+        }]
+    },
+    {
+        placeName: "Vietnam (Dalat City)",
+        LatLng: [{
+            lat: 11.936230,
+            lng: 108.445259
+        }]
+    },
+    {
+        placeName: "Vietnam (Hue City)",
+        LatLng: [{
+            lat: 16.463713,
+            lng: 107.590866
+        }]
+    },
+     {
+        placeName: "Vietnam (Hanoi Cooking Centre)",
+        LatLng: [{
+            lat: 21.054374,
+            lng: 105.837032
+        }]
+    },
+    {
+        placeName: "Vietnam (Hanoi, Capital City)",
+        LatLng: [{
+            lat: 21.027763,
+            lng: 105.834160
+        }]
+    }
 ];
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 5,
-        center: {
-            lat: 16.058324,
-            lng: 108.277000
-        }
-    });
 
-    // Create an array of alphabetical characters used to label the markers.
-    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+window.onload = function() {
+    initMap();
+};
 
-    // Add some markers to the map.
-    // Note: The code uses the JavaScript Array.prototype.map() method to
-    // create an array of markers based on a given "locations" array.
-    // The map() method here has nothing to do with the Google Maps API.
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
+function addMarker() {
+    for (var i = 0; i < markersOnMap.length; i++) {
+        var contentString = '<div id="content"><h1>' + markersOnMap[i].placeName +
+            '</h1><p>Lorem ipsum dolor sit amet, vix mutat posse suscipit id, vel ea tantas omittam detraxit.</p></div>';
+
+        const marker = new google.maps.Marker({
+            position: markersOnMap[i].LatLng[0],
+            map: map
         });
-    });
 
-    // Add a marker clusterer to manage the markers.
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+        const infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 200
+        });
 
-    var request = {
-        placeId: 'ChIJLUKBJJerNTERCH5DEUBZgLg',
-        fields: ['name', 'formatted_address', 'place_id', 'geometry']
-    };
-
-    var infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
-
-    service.getDetails(request, function(place, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            var marker = new google.maps.Marker({
-                map: map,
-                position: place.geometry.location
-            });
-            google.maps.event.addListener(marker, 'click', function() {
-                infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-                    'Place ID: ' + place.place_id + '<br>' +
-                    place.formatted_address + '</div>');
-                infowindow.open(map, this);
-            });
-        }
-    });
+        marker.addListener('click', function() {
+            closeOtherInfo();
+            infowindow.open(marker.get('map'), marker);
+            InforObj[0] = infowindow;
+        });
+        // marker.addListener('mouseover', function () {
+        //     closeOtherInfo();
+        //     infowindow.open(marker.get('map'), marker);
+        //     InforObj[0] = infowindow;
+        // });
+        // marker.addListener('mouseout', function () {
+        //     closeOtherInfo();
+        //     infowindow.close();
+        //     InforObj[0] = infowindow;
+        // });
+    }
 }
 
+function closeOtherInfo() {
+    if (InforObj.length > 0) {
+        /* detach the info-window from the marker ... undocumented in the API docs */
+        InforObj[0].set("marker", null);
+        /* and close it */
+        InforObj[0].close();
+        /* blank the array */
+        InforObj.length = 0;
+    }
+}
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: centerCords
+    });
+    addMarker();
+}
 
